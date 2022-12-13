6415f8f49a02ce464c5e6b7430ff1b1225ed6ec8
JalleliYassine <jalleli.yassine@gmail.com>
2022-12-13 01:08:36 +0100

    timer

--- cookieP.py
+++ cookieP.py
@@ -1,25 +1,21 @@
 import turtle
 import numpy as np
 import random
+import time
 
 wn=turtle.Screen()
 wn.title("cookie with probability")
 wn.bgcolor("black")
 
 wn.register_shape("C:\\Users\\User\\Desktop\\cookie.gif")
-wn.register_shape("C:\\Users\\User\\Desktop\\cookie2.gif")
 
 
 cookie = turtle.Turtle()
-cookie2 = turtle.Turtle()
 
 
+cookie.shape("C:\\Users\\User\\Desktop\\cookie.gif")
 
-c=cookie.shape("C:\\Users\\User\\Desktop\\cookie.gif")
-#y=cookie2.shape("C:\\Users\\User\\Desktop\\cookie2.gif")
-#np.random.choice([c,y],p=[0.8,0.2])
 cookie.speed(0)
-#cookie2.speed(0)
 
 
 clicks = 0
@@ -37,16 +33,48 @@ def clicked(x,y):
     global clicks
     h=[1,2,5]
     
-    #p=random.choices(o,weights=(60,35,15),k=3)
     clicks += np.random.choice(h,p=[0.6,0.3,0.1])
-    #clicks += (random.choices(h,weights=(60,30,10),k=3))
+   
     pen.clear()
     pen.write(f"Clicks: {clicks}", align="center", font = ("Courier New",32,"normal"))
  
-        
 
 cookie.onclick(clicked)
-#cookie2.oneclick(clicked)
-#cookie2.onclick(clicked)
 
-wn.mainloop()
+
+is_paused = False
+
+def toggle_pause():
+    global is_paused
+    if is_paused == True:
+        is_paused = False
+    else:
+        is_paused=True
+
+wn.listen()
+wn.onkeypress(toggle_pause,"p")            
+
+time_limit = 8
+start_time = time.time()
+
+while True: 
+
+
+    if not is_paused:
+     elapsed_time = time.time() - start_time
+     print(time_limit - int(elapsed_time))
+    if elapsed_time > time_limit:
+    
+     turtle.write("GAME OVER",align="center", font = ("Courier New",70,"normal"))
+     exit()
+    if clicks = 80:
+      start_time = time.time()-elapsed_time + 1   
+    if clicks >= 100:
+     turtle.write("WELL PLAYED",align="center", font = ("Courier New",70,"normal")) 
+     exit()
+    else:
+     start_time= time.time()-elapsed_time
+     wn.update()
+
+
+
